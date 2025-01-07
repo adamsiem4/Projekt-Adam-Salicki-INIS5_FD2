00ab61da1cce76d2dcd1d5fd2bee19b57ecd9d80
adamsiem4 <asalicki65@gmail.com>
2025-01-07 23:19:44 +0100

    3

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,36 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
-    entry: './src/index.js', // JavaScript entry point
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js', // Output JS bundle
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/, // Process SASS/SCSS files
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: /\.js$/, // Process JavaScript files
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html', // Use src/index.html as a template
-            filename: 'index.html', // Output to dist/index.html
-        }),
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: 'dist/css', to: 'css' }, // Copy css folder to dist/css
-            ],
-        }),
-    ],
-    mode: 'development',
-};
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+		filename: "bundle.js",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: "babel-loader",
+				},
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "index.html",
+		}),
+		new CopyWebpackPlugin({
+			patterns: [{ from: "dist/css", to: "css" }],
+		}),
+	],
+	mode: "development",
+}
